6feef133f13b6ad6bf61c8b07fa4c0bd3fbefb04
Parvin Sattorova <nanajon66@gmail.com>
2023-08-24 20:03:52 -0600

    refac: Add  error-handling after receiving data from api

--- src/Components/ApiCalls.js
+++ src/Components/ApiCalls.js
@@ -1,6 +1,18 @@
+// export function getMovies() {
+//   return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+//   .then(res => res.json())
+//   // .then(data => data)
+// }
+
 export function getMovies() {
   return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-  .then(res => res.json())
-  // .then(data => data)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
-
