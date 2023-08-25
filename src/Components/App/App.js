38e805dc89f2471f4a2e74a9536f210a3ba2886e
Denzel Turambi <dturambi26@gmail.com>
2023-08-24 18:36:38 -0600

    feat: Fetch API data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,19 @@
-// import logo from './logo.svg';
 import './App.css';
+import { getMovies } from '../ApiCalls';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies()
+    .then(data => setMovies(data.movies))
+  }, [])
+
+  console.log(movies)
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Rancid Tomatillos</h1>
+  )
 }
 
 export default App;
