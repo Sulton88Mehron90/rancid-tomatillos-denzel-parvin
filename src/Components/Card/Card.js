e6d810ce2c1bdc2b3e7ff6ae22f4c532e973c337
Denzel Turambi <dturambi26@gmail.com>
2023-08-25 16:23:02 -0600

    refactor: Refactor rating to be out of 10

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,7 @@ function Card({title, img, rating, id}) {
     <div id={id}>
       <img id={id} src={img} alt={`Poster of ${title}`} />
       <h3 id={id}>{title}</h3>
-      <h3 id={id}>{rating}</h3>
+      <h3 id={id}>{rating}/10</h3>
     </div>
   )
 }
