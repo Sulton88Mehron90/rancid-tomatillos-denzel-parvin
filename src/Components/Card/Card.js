41bea5fb1c1ac2dbc1a08a7abe17d8d2eb310cbf
Parvin Sattorova <nanajon66@gmail.com>
2023-08-27 08:22:16 -0600

    feat:Add website name in index.html add a frame around each card on the hover and a glow if selected, welcome note in navbar

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,16 @@
 import './Card.css';
 // import redTomato from '../../images/redTomato.png';
 import Tomatillo from '../../images/Tomatillo.png'
+import React, { useState } from 'react';
 
 function Card({title, img, rating, id}) {
- 
+  const [isSelected, setIsSelected] = useState(false);
   const tomatilloImages = Array(Math.floor(rating)).fill(null);
+  const cardClass = isSelected ? "glowing-border" : "";
 
   return (
-    <div id={id}>
+    // <div id={id}>
+    <div id={id} className={cardClass} onClick={() => setIsSelected(!isSelected)}>
       <img id={id} src={img} alt={`Poster of ${title}`} />
       <h3 id={id}>{title}</h3>
       <div className="rating-container">
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,6 +8,8 @@ const Navbar = () => {
       <div className="nav-content">
       <h1 className="nav-title">Rancid Tomatillos</h1>
         <img src={Tomatillo} className="nav-logo" alt="Tomatillo logo" />
+        <span className="welcome-note">Ripe or Rancid, We've Got the Flicks!</span>
+        {/* <span className="welcome-note">Welcome!</span> */}
         {/* <img src={redTomato} className="nav-logo" alt="Red Tomato logo" /> */}
       </div>
     </nav>
