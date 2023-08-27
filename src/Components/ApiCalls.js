f61acfd7c5388c36ff646ebd638855edb418c0c5
Parvin Sattorova <nanajon66@gmail.com>
2023-08-26 23:49:41 -0600

    feat:Add Navbar js and css files component. add tomatilloImages function in Card componenet. refac the api file.

--- src/Components/ApiCalls.js
+++ src/Components/ApiCalls.js
@@ -1,9 +1,3 @@
-// export function getMovies() {
-//   return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-//   .then(res => res.json())
-//   // .then(data => data)
-// }
-
 export function getMovies() {
   return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
     .then(response => {
