449c247bb375eaf64dabaefb84a896430921fd11
Denzel Turambi <dturambi26@gmail.com>
2023-08-27 15:21:22 -0600

    feat: Add NavLink to logo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import './Navbar.css'
 import Tomatillo from '../../images/Tomatillo.png'
 // import redTomato from '../../images/redTomato.png'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <div className="nav-content">
-      <h1 className="nav-title">Rancid Tomatillos</h1>
+        <NavLink to="/" style={{textDecoration:'none'}}>
+        <h1 className="nav-title">Rancid Tomatillos</h1>
+        </NavLink>
         <img src={Tomatillo} className="nav-logo" alt="Tomatillo logo" />
         <span className="welcome-note">Ripe or Rancid, We've Got the Flicks!</span>
         {/* <span className="welcome-note">Welcome!</span> */}
