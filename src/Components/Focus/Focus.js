d49b5096f20030eb530c8e4a18deba1a113c489c
Parvin Sattorova <nanajon66@gmail.com>
2023-08-27 20:13:34 -0600

    feat: Add css file to Focus componenet

--- src/Components/Focus/Focus.js
+++ src/Components/Focus/Focus.js
@@ -1,6 +1,8 @@
 import { getSingleMovie } from "../ApiCalls";
 import { useEffect, useState } from "react";
 import { useParams, NavLink } from "react-router-dom";
+import './Focus.css';
+
 
 function Focus() {
   const [singleMovie, setSingleMovie] = useState({})
@@ -10,24 +12,27 @@ function Focus() {
 
   useEffect(() => {
     getSingleMovie(movieID.id)
-    .then(data => setSingleMovie(data.movie))
+      .then(data => setSingleMovie(data.movie))
   }, [])
 
-  console.log("singleMovie after Fetch",singleMovie)
+  console.log("singleMovie after Fetch", singleMovie)
 
   return (
-    <div id={singleMovie.id}>
-      <h2>{singleMovie.title}</h2>
-      <h3>{singleMovie.tagline}</h3>
-      <p>overview: {singleMovie.overview}</p>
-      <p>average rating: {singleMovie.average_rating}/10</p>
-      <p>runtime: {singleMovie.runtime} minutes</p>
-      <p>genre(s): {singleMovie.genres}</p>
-      <p>release date: {singleMovie.release_date}</p>
-      <NavLink to="/"><button>Go Back</button></NavLink>
-      <img src={singleMovie.backdrop_path} alt={singleMovie.title} />
-    </div>
-  )
-}
-
+    <div id="focus-container" style={{ 
+      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('${singleMovie.backdrop_path}')`
+    }}>
+      <div id="focus-text-content">
+        <h2>{singleMovie.title}</h2>
+        <h3>{singleMovie.tagline}</h3>
+        <p><span className="label">overview:</span> {singleMovie.overview}</p>
+        <p><span className="label">average rating:</span> {singleMovie.average_rating}/10</p>
+        <p><span className="label">runtime:</span> {singleMovie.runtime} minutes</p>
+        <p><span className="label">genre(s):</span> {singleMovie.genres}</p>
+        <p><span className="label">release date:</span> {singleMovie.release_date}</p>
+        <NavLink to="/"><button>Go Back</button></NavLink>
+        </div>
+    <img src={singleMovie.backdrop_path} alt={singleMovie.title} />
+  </div>
+);
+};
 export default Focus;
