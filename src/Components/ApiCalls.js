f62a84c05df32c75533a36833d7a36e59b822a47
Parvin Sattorova <nanajon66@gmail.com>
2023-08-28 21:26:48 -0600

    refactor: Add Error Propagation: By removing the .catch() block in the getMovies() and getSingleMovie() functions, you've allowed the error to propagate up to the useEffect() where it's caught and then used to set the error state.

--- src/Components/ApiCalls.js
+++ src/Components/ApiCalls.js
@@ -1,25 +1,30 @@
 export function getMovies() {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/')
     .then(response => {
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(response.status.toString());
       }
       return response.json();
     })
-    .catch(error => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+    // .catch(error => {
+    //   console.error("There was a problem with the fetch operation:", error);
+    // });
 }
 
 export function getSingleMovie(id) {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .catch(error => {
-    console.error("There was a problem with the fetch operation:", error);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status.toString());
+      }
+      return response.json();
+    })
+    // .catch(error => {
+    //   console.error("There was a problem with the fetch operation:", error);
+    // });
 }
+
+
+// Error Propagation: By removing the .catch() block in the getMovies() and getSingleMovie() functions, you've allowed the error to propagate up to the useEffect() where it's caught and then used to set the error state.
+
+// Centralized Error Handling: Centralizing your error handling allows you to manage all types of errors in one place. It makes it easier to show error messages or error pages.
