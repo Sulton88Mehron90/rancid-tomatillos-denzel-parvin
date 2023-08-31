84745006f33e4d8a5dcd65608cf277d7b0379e9c
Denzel Turambi <dturambi26@gmail.com>
2023-08-31 16:48:38 -0600

    feat: Implement searchVisible prop

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,28 +2,43 @@ import './Navbar.css'
 import Tomatillo from '../../images/Tomatillo.png'
 import { NavLink } from 'react-router-dom';
 
-const Navbar = ({search, searchFilter}) => {
-  return (
-    <nav>
-      <div className="nav-content">
-        <NavLink to="/" style={{textDecoration:'none'}}>
-          <h1 className="nav-title">Rancid Tomatillos</h1>
-          <img src={Tomatillo} className="nav-logo" alt="Tomatillo logo" />
-        </NavLink>
-        <form>
-          <input 
-          id="search-input"
-          type="text"
-          placeholder="Search for movies..."
-          name={search}
-          value={search}
-          onChange={searchFilter}
-          />
-        </form>
-        <span className="welcome-note">Welcome Friend!</span>
-      </div>
-    </nav>
-  );
+const Navbar = ({search, searchFilter, searchVisible}) => {
+  if(searchVisible){
+    return (
+      <nav>
+        <div className="nav-content">
+          <NavLink to="/" style={{textDecoration:'none'}}>
+            <h1 className="nav-title">Rancid Tomatillos</h1>
+            <img src={Tomatillo} className="nav-logo" alt="Tomatillo logo" />
+          </NavLink>
+          <form>
+            <input 
+            id="search-input"
+            type="text"
+            placeholder="Search for movies..."
+            name={search}
+            value={search}
+            onChange={searchFilter}
+            />
+          </form>
+          <span className="welcome-note">Welcome Friend!</span>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav>
+        <div className="nav-content">
+          <NavLink to="/" style={{textDecoration:'none'}}>
+            <h1 className="nav-title">Rancid Tomatillos</h1>
+            <img src={Tomatillo} className="nav-logo" alt="Tomatillo logo" />
+          </NavLink>
+          <span className="welcome-note">Welcome Friend!</span>
+        </div>
+      </nav>
+    );
+  }
+
 }
 
 export default Navbar;
